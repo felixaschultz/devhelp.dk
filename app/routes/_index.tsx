d7ad9857ddac247ccfa51681c5dc313fb489c9da
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-13 20:38:34 +0100

    Add blog posts to index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,10 +3,14 @@ import { Banner } from "../components/Banner";
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/node";
 import { authenticator } from "~/services/auth.server";
+import PostCard from "~/components/PostCard";
+import mongoose from "mongoose";
 
 export const loader = async ({ request }) => {
   const user = await authenticator.isAuthenticated(request);
-  return { user };
+  const blogPosts = await mongoose.model("BlogPost").find();
+
+  return { user, blogPosts};
 };
 
 export const meta: MetaFunction = () => {
@@ -17,13 +21,22 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const { user } = useLoaderData();
+  const { user, blogPosts } = useLoaderData();
 
   return (
     <>
       <Banner user={user} />
       <section className="content">
         <p>Welcome to Devhelp by Intastellar Solutiuons, International</p>
+        <h2>Blog</h2>
+        <p>Read the latest blog posts</p>
+        <section className="blog-grid">
+          {(blogPosts) ? blogPosts.map((post) => (
+            <PostCard key={post._id} post={post} />
+          )) : (
+            <p>No blog posts found</p>
+          )}
+        </section>
       </section>
     </>
   );
