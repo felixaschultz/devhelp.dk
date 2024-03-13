14e25efb7c58b126c82cb297f2d71877c4cdf338
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-13 20:00:06 +0100

    Refactor user data in Me component

--- app/routes/me.$id.tsx
+++ app/routes/me.$id.tsx
@@ -13,7 +13,7 @@ export const loader = async ({ request, params }) => {
         throw new Error("User not found");
     }
 
-    return { user, userData };
+    return { userData };
 }
 export const meta = [
     { title: "Me | Devhelp.dk" },
@@ -21,11 +21,12 @@ export const meta = [
 ];
 
 export default function Me() {
-    const { user, userData } = useLoaderData();
+    const { userData } = useLoaderData();
     return (
         <div className="content">
-            <img className="userProfile" src={user.image || "https://scontent-uc-d2c-7.intastellaraccounts.com/a/s/ul/p/avtr46-img/felix.schultz@intastellar.com/profile/i3ek74fxmlnpeeazw6wadfk6lhxealofk7z6391v8a60reol0uyf4w7vic9jab2xjzmix1d3otvrsj2bv6i604id2j5j0v9nm0vlb9qv3wfb26tvw4otd0n8q49ugm4e3ew4rikm7di8qco0w33kz03nmz0r45g0bos12sbk2vra7vdmw8ewpkydo97y8f1ycr4i82eu.jpg"} alt="" />
-            <h1>{user.name.firstname}</h1>
+            <img className="userProfile" src={userData.image || "https://scontent-uc-d2c-7.intastellaraccounts.com/a/s/ul/p/avtr46-img/felix.schultz@intastellar.com/profile/i3ek74fxmlnpeeazw6wadfk6lhxealofk7z6391v8a60reol0uyf4w7vic9jab2xjzmix1d3otvrsj2bv6i604id2j5j0v9nm0vlb9qv3wfb26tvw4otd0n8q49ugm4e3ew4rikm7di8qco0w33kz03nmz0r45g0bos12sbk2vra7vdmw8ewpkydo97y8f1ycr4i82eu.jpg"} alt="" />
+            <h1>{userData.name.firstname} {userData.name.lastname}</h1>
+            <p>{userData.email}</p>
         </div>
     );
 }
