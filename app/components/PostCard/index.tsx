8adb3d0bdbdc91dcefbba70793c18caa578c4215
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-13 22:34:44 +0100

    Add draft indicator to PostCard component

--- app/components/PostCard/index.tsx
+++ app/components/PostCard/index.tsx
@@ -4,6 +4,7 @@ import { Link } from "@remix-run/react";
 export default function PostCard({post, user}) {
     return (
         <div className="post-card">
+            {post.published ? null : <p style={{color: "red", marginTop: 0}}>Draft</p>}
             {post.image && <img src={post.image} alt={post.title} />}
             <h2>{post.title}</h2>
             <p>{post.body}</p>
