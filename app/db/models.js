6fe1dadc20ea823d7e502f7727a1626d585edb60
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-13 18:21:02 +0100

    Add user profile image and name to header

--- app/db/models.js
+++ app/db/models.js
@@ -20,7 +20,10 @@ const userSchema = new Schema({
             required: true
         }
     },
-    image: String,
+    image: {
+        type: String,
+        default: "https://scontent-uc-d2c-7.intastellaraccounts.com/a/s/ul/p/avtr46-img/felix.schultz@intastellar.com/profile/i3ek74fxmlnpeeazw6wadfk6lhxealofk7z6391v8a60reol0uyf4w7vic9jab2xjzmix1d3otvrsj2bv6i604id2j5j0v9nm0vlb9qv3wfb26tvw4otd0n8q49ugm4e3ew4rikm7di8qco0w33kz03nmz0r45g0bos12sbk2vra7vdmw8ewpkydo97y8f1ycr4i82eu.jpg"
+    },
     role: {
         type: String,
         default: "user"
