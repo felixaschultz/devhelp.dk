e45734345f3dd651d813df242ed3560000c13d13
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-13 18:44:40 +0100

    Update Footer styling and content

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -2,7 +2,8 @@ import "./Style.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Devhelp.dk | Intastellar Solutions, International. All rights reserved.</p>
+        <p className="footerText">This is a Intastellar Solutions, International Project.</p>
+        <p className="copy">&copy; { new Date().getFullYear() } Intastellar Solutions, International. All rights reserved.</p>
     </footer>
   );
 }
