41a0d2454ad9c80400f89d953643060dae6c13c0
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-14 12:48:16 +0100

    Fix user profile layout for mobile devices

--- app/routes/me.$id.tsx
+++ app/routes/me.$id.tsx
@@ -41,7 +41,7 @@ export default function Me() {
 
     return (
         <div className="profile-grid content">
-            <section>
+            <section className="userInfos">
                 <div className="profileImage-container">
                     <img className="userProfile" src={userData?.image || "https://scontent-uc-d2c-7.intastellaraccounts.com/a/s/ul/p/avtr46-img/felix.schultz@intastellar.com/profile/i3ek74fxmlnpeeazw6wadfk6lhxealofk7z6391v8a60reol0uyf4w7vic9jab2xjzmix1d3otvrsj2bv6i604id2j5j0v9nm0vlb9qv3wfb26tvw4otd0n8q49ugm4e3ew4rikm7di8qco0w33kz03nmz0r45g0bos12sbk2vra7vdmw8ewpkydo97y8f1ycr4i82eu.jpg"} alt="" />
                     {user?._id === userData?._id && (
@@ -60,7 +60,7 @@ export default function Me() {
                     )
                 }
             </section>
-            <section>
+            <section className="userContent">
                 <h2>{userData.name.firstname}´s top 3 popular blog posts</h2>
                 <section className="blog-grid --popular">
                     {userPosts.map(post => ({
