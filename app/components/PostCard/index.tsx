e3710febc74f7aca4b2250d64f0895b5b83247d2
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-14 21:48:55 +0100

    Add onLoadStart event to hide image while loading

--- app/components/PostCard/index.tsx
+++ app/components/PostCard/index.tsx
@@ -6,7 +6,9 @@ export default function PostCard({post, user}) {
         <div className="post-card">
             {(user?._id == post.user && post?.popularityScore) ? <p>Popularity Score: {post?.popularityScore}</p> : null}
             {post.published ? null : <p style={{color: "red", marginTop: 0}}>Draft</p>}
-            {post.image && <img src={post.image} alt={post.title} />}
+            {post.image && <img onLoadStart={(e) => {
+                e.target.style.display = "none";
+            }} src={post.image} alt={post.title} />}
             <h2>{post.title}</h2>
             <p>{post.body}</p>
             {
