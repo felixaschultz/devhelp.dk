40d40de43c98cfd0dfbefe33510edbb3c4cc4cd5
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-14 16:07:30 +0100

    Update ProUser.css and ask.tsx

--- app/routes/ask.tsx
+++ app/routes/ask.tsx
@@ -1,7 +1,7 @@
 import { Form, useFetcher, useLoaderData, useLocation, useOutletContext } from "@remix-run/react";
 import { useEffect, useState } from "react";
 import { authenticator } from "~/services/auth.server";
-import mongoose from "mongoose";
+import mongoose, { set } from "mongoose";
 import "../ProUser.css";
 
 export const loader = async ({ request }) => {
@@ -22,6 +22,7 @@ export default function Ask() {
     const [openAskForm, setOpenAskForm] = useState(false);
     const [askButtonClicked, setAskButtonClicked] = useState(false);
     const { proUsers, user } = useLoaderData();
+    const [proUser, setProUser] = useState(proUsers);
     const [open, setOpen] = useOutletContext();
     const location = useLocation();
 
@@ -52,30 +53,42 @@ export default function Ask() {
         <>
             <div className="content">
                 <h1>Ask a professional</h1>
-                    {proUsers.map(user => (
-                        <div key={user._id}>
-                            <img className="proUser-image" src={user.image} alt={user.name.firstname} />
-                            <h2>{user.name.firstname} {user.name.lastname}</h2>
-                            <button className="btn" onClick={handleClicked}>Ask {user.name.firstname} for help</button>
-                            {
-                                user.skills.map((skill, index) => (
-                                    <p key={index}>
-                                        {skill.name} - {skill.level}
-                                    </p>
-                                ))
-                            }
-                        </div>
-                    
-                    ))}
-                    {(openAskForm) && (
-                        <Form method="post" action="/ask">
-                            <h2>Ask a professional</h2>
-                            <input type="hidden" name="to" value={proUsers?._id} />
-                            <label htmlFor="question">Question</label>
-                            <textarea id="question" name="question" placeholder={`Write your Question here...`} />
-                            <button className="btn" name="_action" value="" type="submit">Ask</button>
-                        </Form>
-                    )}
+                <p>Here are the professionals that you can ask for help.</p>
+                <section className="filter">
+                    <input className="input-fields" placeholder="Search after a specific user or skill" type="search" onChange={(e) => {
+                        setProUser(proUsers.filter(
+                            user => 
+                                user.name.firstname.toLowerCase().includes(e.target.value.toLowerCase()) 
+                                || user.name.lastname.toLowerCase().includes(e.target.value.toLowerCase())
+                                || user.skills.some(skill => skill.name.toLowerCase().includes(e.target.value.toLowerCase())
+                            )
+                        ));
+                    }} />
+                </section>
+                {proUser.map(user => (
+                    <div key={user._id}>
+                        <img className="proUser-image" src={user.image} alt={user.name.firstname} />
+                        <h2>{user.name.firstname} {user.name.lastname}</h2>
+                        <button className="btn" onClick={handleClicked}>Ask {user.name.firstname} for help</button>
+                        {
+                            user.skills.map((skill, index) => (
+                                <p key={index}>
+                                    {skill.name} - {skill.level}
+                                </p>
+                            ))
+                        }
+                    </div>
+                
+                ))}
+                {(openAskForm) && (
+                    <Form method="post" action="/ask">
+                        <h2>Ask a professional</h2>
+                        <input type="hidden" name="to" value={proUsers?._id} />
+                        <label htmlFor="question">Question</label>
+                        <textarea id="question" name="question" placeholder={`Write your Question here...`} />
+                        <button className="btn" name="_action" value="" type="submit">Ask</button>
+                    </Form>
+                )}
             </div>
         </>
     );
