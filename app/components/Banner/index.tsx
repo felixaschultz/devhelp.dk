627dad41ec1ed73dd78e73e0093fefad5073e2f8
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-14 15:23:40 +0100

    Refactor search section in Banner component

--- app/components/Banner/index.tsx
+++ app/components/Banner/index.tsx
@@ -11,22 +11,24 @@ function Banner({user}) {
                 <div className="hero-tagline">
                     <img className="hero-logo" src={Logo} alt="DevHelp Logo" />
                     <p>Står du med udfordinger af dit projekt? Så står vores dygtige udvikler klar til at hjælpe dig.</p>
-                    {(!user) ? (
-                        <div className="cta">
-                            <button className="btn" onClick={() => {
-                                setOpen(
-                                    {
-                                        open: true,
-                                        type: "signup"
-                                    }
-                                );
-                            }}>Start i dag</button>
-                        </div>
-                    ): null}
-                    <Form className="flex" action="/search" method="GET">
-                        <input className="input-fields" type="text" name="q" placeholder="Søg for hjælp" />
-                        <button className="btn" type="submit">Søg</button>
-                    </Form>
+                    <section className="flex">
+                        {(!user) ? (
+                            <div className="cta">
+                                <button className="btn" onClick={() => {
+                                    setOpen(
+                                        {
+                                            open: true,
+                                            type: "signup"
+                                        }
+                                    );
+                                }}>Start i dag</button>
+                            </div>
+                        ): null}
+                        <Form className="flex search" action="/search" method="GET">
+                            <input className="input-fields" type="text" name="q" placeholder="Søg for hjælp" />
+                            <button className="btn" type="submit">Søg</button>
+                        </Form>
+                    </section>
                 </div>
                 <div>
                     <img src="https://via.placeholder.com/1000x550" alt="placeholder" />
