66868cb0a84bfc52764f23796d32c8eb99d100ca
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-14 17:11:59 +0100

    Add search functionality and display search results

--- app/routes/search.tsx
+++ app/routes/search.tsx
@@ -1,5 +1,6 @@
-import {useLoaderData, Link} from "@remix-run/react";
+import {useLoaderData, Link, Form} from "@remix-run/react";
 import mongoose from "mongoose";
+import "../App.css";
 
 export const loader = async ({request, params}) => {
     const q = request.url.split("?q=")[1];
@@ -16,6 +17,29 @@ export const loader = async ({request, params}) => {
 
     const questions = await mongoose.model("Question").find({
         question: new RegExp(q, "i")
+    });
+
+    const users = await mongoose.model("User").find({
+        $or: [
+            {"name.firstname": new RegExp(q, "i")},
+            {"name.lastname": new RegExp(q, "i")},
+            {
+                "skills": {
+                  $elemMatch: {
+                    key: new RegExp(q, "i")
+                  }
+                }
+            }
+        ]
+    });
+
+    users.forEach(user => {
+        items.push({
+            title: user.name.firstname + " " + user.name.lastname,
+            description: user.skills,
+            url: `/me/${user._id}`,
+            type: "user"
+        });
     });
 
     questions.forEach(item => {
@@ -43,13 +67,18 @@ export default function Search(){
     const {q, items} = useLoaderData();
     return (
         <div className="content">
+            <Form className="flex search" action="/search" method="GET">
+                <input className="input-fields" type="text" name="q" defaultValue={q} placeholder="Søg for hjælp" />
+                <button className="btn" type="submit">Søg</button>
+            </Form>
             <h1>Search results for {q}</h1>
             <p>Found {items.length} results</p>
             {items.length === 0 && <p>No results found</p>}
-            <ul>
+            <ul className="search-results">
                 {items.map(item => (
                     <li key={item.url}>
                         <Link to={item.url}>
+                            <h2>{item.type}</h2>
                             <p>{item.title}</p>
                         </Link>
                     </li>
