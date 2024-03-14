74986a71074f3d5c45e6ef0cb6dc7e8df0b1d8c5
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-14 09:40:00 +0100

    Refactor Comments component styling and structure

--- app/components/Comments/index.tsx
+++ app/components/Comments/index.tsx
@@ -5,8 +5,6 @@ import { useState } from "react";
 export default function Comments({ post, user }) {
     const fetcher = useFetcher();
     const [newReply, setReply] = useState(false);
-
-    console.log(post.comments);
     
     return (
         <div className="commentSection">
@@ -22,48 +20,51 @@ export default function Comments({ post, user }) {
             </fetcher.Form>
             <div className="comments">
                 <h2>Comments</h2>
-                {post?.comments?.length === 0 ? <p>No comments yet</p> : post?.comments?.map((comment, index) => (
-                    <>
-                        <div key={index} className="comment">
-                            <p>{comment.body}</p>
-                            <section className="comment-reply">
-                                <p><img src={comment.user.image} alt="" className="comment-profileImage" /> {comment.user.name.firstname}</p>
-                                <button className="reply_btn" onClick={ () => {
-                                    setReply(!newReply)
-                                }}>Reply</button>
-                            </section>
-                        </div>
-                        {
-                            newReply && (
-                                <fetcher.Form method="post">
-                                    <div>
-                                        <label htmlFor="reply">Reply</label>
-                                        <textarea className="input-fields textarea comment" id="reply" name="body" />
-                                        <input type="hidden" name="user" value={user._id} />
-                                        <input type="hidden" name="commentId" value={comment._id} />
-                                    </div>
-                                    <section className="flex">
-                                        <button name="_action" value="reply" className="post-btn" type="submit">Reply</button>
+                    {post?.comments?.length === 0 ? <p>No comments yet</p> : post?.comments?.map((comment, index) => (
+                        <>
+                            <div className="comment-group">
+                                <div key={index} className="comment">
+                                    <p>{comment.body}</p>
+                                    <section className="comment-reply">
+                                        <p className="user"><img src={comment.user.image} alt="" className="comment-profileImage" /> {comment.user.name.firstname}</p>
+                                        <button className="reply_btn" onClick={ () => {
+                                            setReply(!newReply)
+                                        }}>Reply</button>
                                     </section>
-                                </fetcher.Form>
-                            )
-                        }
-                        {
-                            (comment.reply.length > 0) && (
-                                <div className="replies">
-                                    {comment.reply.map((reply, index) => (
-                                        <div key={index} className="comment reply">
-                                            <p>{reply.body}</p>
-                                            <p><img src={comment.user.image} alt="" className="comment-profileImage" /> {comment.user.name.firstname}</p>
-                                        </div>
-                                    ))}
                                 </div>
-                            )
-                        }
-                    </>
-                )).sort((a, b) => {
-                    return new Date(b.date) - new Date(a.date);
-                })}
+                                {
+                                    newReply && (
+                                        <fetcher.Form method="post">
+                                            <div>
+                                                <label htmlFor="reply">Reply</label>
+                                                <textarea className="input-fields textarea comment" id="reply" name="body" />
+                                                <input type="hidden" name="user" value={user._id} />
+                                                <input type="hidden" name="commentId" value={comment._id} />
+                                            </div>
+                                            <section className="flex">
+                                                <button name="_action" value="reply" className="post-btn" type="submit">Reply</button>
+                                            </section>
+                                        </fetcher.Form>
+                                    )
+                                }
+                                {
+                                    (comment.reply.length > 0) && (
+                                        <div className="replies">
+                                            {comment.reply.map((reply, index) => (
+                                                <div key={index} className="comment reply">
+                                                    <p>{reply.body}</p>
+                                                    <p className="user"><img src={comment.user.image} alt="" className="comment-profileImage" /> {comment.user.name.firstname}</p>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        </>
+                        
+                    )).sort((a, b) => {
+                        return new Date(b.date) - new Date(a.date);
+                    })}
             </div>
         </div>
     );
