ecc2cee1ba97fd609f92e73338841daf22f7dd30
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-14 23:04:32 +0100

    Add link to official Github in Footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -10,6 +10,7 @@ export default function Footer() {
             <Link to="/terms-of-service">Terms of Service</Link>
             <Link to="/imprint">Imprint</Link>
             <a href="https://github.com/felixaschultz/devhelp.dk/issues/new" rel="noopener" target="_blank">Feedback</a>
+            <a href="https://github.com/felixaschultz/devhelp.dk" rel="noopener" target="_blank">Official Github</a>
         </section>
         <p className="copy">&copy; { new Date().getFullYear() } Intastellar Solutions, International. All rights reserved.</p>
     </footer>
