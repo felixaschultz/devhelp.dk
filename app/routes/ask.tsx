eae6cdcd2c82782aff4f5a46fa2f1df5a393ce36
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-14 15:39:19 +0100

    Add ask form and toggle functionality

--- app/routes/ask.tsx
+++ app/routes/ask.tsx
@@ -1,4 +1,5 @@
 import { Form, useFetcher, useLoaderData, useOutletContext } from "@remix-run/react";
+import { useState } from "react";
 import { authenticator } from "~/services/auth.server";
 import mongoose from "mongoose";
 import "../ProUser.css";
@@ -18,11 +19,12 @@ export const meta = [
 
 export default function Ask() {
     const fetcher = useFetcher();
+    const [openAskForm, setOpenAskForm] = useState(false);
     const { proUsers, user } = useLoaderData();
-
     const [open, setOpen] = useOutletContext();
 
-    function handleClicked() {
+    function handleClicked(e) {
+        e.preventDefault();
         if(!user){
             setOpen(
                 {
@@ -30,29 +32,40 @@ export default function Ask() {
                     type: "login"
                 }
             );
+        }else{
+            setOpenAskForm(!openAskForm)
         }
 
     }
 
     return (
-        <div className="content">
-            <h1>Ask a professional</h1>
-            {proUsers.map(user => (
-                <div key={user._id}>
-                    <img className="proUser-image" src={user.image} alt={user.name.firstname} />
-                    <h2>{user.name.firstname} {user.name.lastname}</h2>
-                    {
-                        user.skills.map((skill, index) => (
-                            <p key={index}>
-                                {skill.name} - {skill.level}
-                            </p>
-                        ))
-                    }
-                    <button className="btn" onClick={handleClicked}>Ask</button>
-                </div>
-            
-            ))}
-        </div>
+        <>
+            <div className="content">
+                <h1>Ask a professional</h1>
+                {proUsers.map(user => (
+                    <div key={user._id}>
+                        <img className="proUser-image" src={user.image} alt={user.name.firstname} />
+                        <h2>{user.name.firstname} {user.name.lastname}</h2>
+                        {
+                            user.skills.map((skill, index) => (
+                                <p key={index}>
+                                    {skill.name} - {skill.level}
+                                </p>
+                            ))
+                        }
+                        <button className="btn" onClick={handleClicked}>Ask {user.name.firstname}</button>
+                    </div>
+                
+                ))}
+                {(openAskForm) && (
+                    <Form method="post" action="/ask">
+                        <input type="hidden" name="to" value={proUsers?._id} />
+                        <textarea name="question" />
+                        <button type="submit">Ask</button>
+                    </Form>
+                )}
+            </div>
+        </>
     );
 }
 
