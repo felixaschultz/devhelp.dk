e900eef0d1265b136e63e002694464d6faae70a8
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-14 22:28:13 +0100

    Revert

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import "./Style.css";
 export default function Footer() {
   return (
     <footer className="footer">
-        <p className="footerText">a Intastellar Solutions, International Project. A non-financial project. Version 1.0.0</p>
+        <p className="footerText">a Intastellar Solutions, International Project. Version 1.0.0</p>
         <p className="copy">&copy; { new Date().getFullYear() } Intastellar Solutions, International. All rights reserved.</p>
     </footer>
   );
