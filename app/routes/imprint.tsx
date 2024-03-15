6229207ff175120254ea8018ef5570c5f1837659
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-15 19:10:57 +0100

    Add imprint information to TermsOfService component

--- app/routes/imprint.tsx
+++ app/routes/imprint.tsx
@@ -25,6 +25,23 @@ export default function TermsOfService(){
     return (
         <div className="content">
             <h1>Impressum</h1>
+
+            <p>
+                Intastellar Solutions, International <br />
+                Risdalsvej 36, st 13. <br />
+                8260 Viby J <br />
+                Denmark <br />
+                CVR: 41604409
+            </p>
+            <p>
+                Email: info@devhelp.dk <br />
+                Web: www.devhelp.dk
+            </p>
+            <p>
+                Personal adherent society: Intastellar Solutions, International <br />
+                Authorized representer: Felix Adrian Schultz <br />
+            </p>
+            <p>Country: Denmark</p>
         </div>
     );
 }
