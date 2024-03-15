35f86638e346ca8e6dfe617c73c8a4e8b355d8e8
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-15 21:34:58 +0100

    Update about page content

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -18,8 +18,11 @@ export const meta = [
 export default function About() {
   return (
     <div className="content">
-      <h1>Om Devhelp.dk</h1>
-      <p>Platformen byder på...</p>
+      <h1>Velkommen til vores platform!</h1>
+      <p>Vi er stolte af at præsentere vores lille lærings SoMe platform, hvor udviklere kan samles, dele viden og lære sammen. Med en lille, men levende blog er vores platform det perfekte sted til artikler og hjælpeartikler om udvikling, inklusive tutorials og meget mere.</p>
+      <p>Vi forstår, at nogle gange kan man stå fast i udviklingsprocessen, og det er her, vores platform virkelig skinner. Ved at tilbyde muligheden for at kontakte professionelle inden for bestemte temaer, såsom Remix, giver vi vores brugere den ekstra støtte, de har brug for, når Google ikke længere er tilstrækkeligt.</p>
+      <p>Men det stopper ikke der. Vores planer inkluderer integration af chatfunktioner, videoer og læringskurser, hvilket vil give vores fællesskab endnu flere værktøjer til at vokse og udvikle sig sammen.</p>
+      <p>Uanset om du er en erfaren udvikler eller lige er begyndt din rejse inden for kodning, er vores platform designet til at være din go-to ressource til alt relateret til udvikling. Vi ser frem til at byde dig velkommen og være en del af din udviklingsrejse!</p>
       <section className="statements">
         <h2>Mission</h2>
         <p>Vi vil hjælpe dig med at finde den rette udvikler til dit projekt. Vi vil have værdi fuldt indhold, som man kan trække lære fra.</p>
