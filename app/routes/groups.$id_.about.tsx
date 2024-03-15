33a59cc3c8b728626d4561d17802a99d026fd2fd
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-15 20:57:31 +0100

    Add navigation tabs to group page

---
+++ app/routes/groups.$id_.about.tsx
@@ -0,0 +1,66 @@
+import { authenticator } from "../services/auth.server";
+import { useLoaderData, useFetcher, Link } from "@remix-run/react";
+import { useEffect, useRef } from "react";
+import mongoose from "mongoose";
+import "../styles/Group.css";
+export const loader = async ({ request, params }) => {
+    const user = await authenticator.isAuthenticated(request);
+    const groups = await mongoose.model("Group").findOne({
+        /* $or: [
+            { creator: new mongoose.Types.ObjectId(user?._id) },
+            { members: new mongoose.Types.ObjectId(user?._id) }
+        ], */
+        /* $and: [ */
+            /* {  */_id: new mongoose.Types.ObjectId(params?.id)/* } */
+        /* ] */
+    }).populate("creator").populate("members.user").populate("posts.user").populate("posts.comments");
+
+    return { user, groups };
+}
+
+export const meta = ({data}) => {
+
+    return [
+        {
+            title: data?.groups?.group_name + " | Devhelp.dk",
+            description: "Group"
+        }
+    ]
+};
+
+export default function GroupAbout() {
+    const { user, groups } = useLoaderData();
+    const memberStatus = groups.members.find(member => member.user == user?._id)?.status;
+    return (
+        <div className="content">
+            <header className="group-info">
+                <section>
+                    <p className="group-type">Gruppe</p>
+                    <h1 className="group-name">{groups.group_name}</h1>
+                    <p className="group-member-info">Gruppen har { groups.members.filter(member => member.status === "accepted").length + 1} medlemmer</p>
+                    <Link className="tab" to={`/groups/${groups._id}`}>Home</Link>
+                    <Link className="tab active" to={`/groups/${groups._id}/about`}>Om</Link>
+                </section>
+                <section>
+                    {
+                        memberStatus === "pending" && (
+                            <p>Du har en anmodning om at blive medlem af gruppen</p>
+                        )
+                    }
+                    {
+                        memberStatus === "accepted" && (
+                            <form method="post">
+                                <button type="submit" name="action" value="leave">Forlad gruppen</button>
+                            </form>
+                        )
+                    }
+                </section>
+            </header>
+            <section>
+                <h2>Beskrivelse</h2>
+                <p>{groups.description}</p>
+            </section>
+        </div>
+    )
+
+}
