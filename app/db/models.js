8ac9a4a782f8dd114307e8f72b7bf795a47cd1e9
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-15 17:34:41 +0100

    Add navigation link to groups and include creator field in StudyGroups schema

--- app/db/models.js
+++ app/db/models.js
@@ -56,6 +56,10 @@ const StudyGroups = new Schema({
         type: String,
         required: true
     },
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
     members: [
         {
             status: {
