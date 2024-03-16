8cf5d7f12eb48bd77bddb5af97df525aacea5937
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-16 13:41:41 +0100

    Add billing, notifications, privacy, and security settings routes

---
+++ app/routes/settings_.billing.tsx
@@ -0,0 +1,37 @@
+import { authenticator } from "../services/auth.server";
+import mongoose from "mongoose";
+import { useLoaderData, Link } from "@remix-run/react";
+import SettingsNav from "../components/SettingsNav";
+import "../styles/Admin-pro.css";
+
+export const loader = async ({request}) => {
+    const user = await authenticator.isAuthenticated(request, {
+        failureRedirect: "/"
+    });
+
+    const userSettings = await mongoose.model("User").findOne({_id: user?._id});
+
+    return {user, userSettings};
+};
+
+export const meta = [
+    {
+        title: "Billing Settings | Devhelp.dk",
+        description: "Questions to me"
+    }
+];
+
+export default function Settings(){
+    const {user, userSettings} = useLoaderData();
+    return (
+        <div className="content grid">
+            <aside>
+                <h1>Billing Settings</h1>
+                <SettingsNav userSettings={userSettings} />
+            </aside>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
