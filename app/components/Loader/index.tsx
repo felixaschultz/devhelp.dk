bf5cf635abac1aeec1324e7eb1edad893fca5a2e
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-16 23:27:27 +0100

    Add loading bar animation to Loader component

--- app/components/Loader/index.tsx
+++ app/components/Loader/index.tsx
@@ -3,12 +3,7 @@ import "./Style.css";
 export default function Loader() {
     return (
         <div className="loader">
-            <div className="loader_container">
-                <div className="loader_content">
-                    <img src={logo} alt="DevHelp Logo" />
-                    <h2>Loading...</h2>
-                </div>
-            </div>
+            <div className="loading-bar" />
         </div>
     )
 }
