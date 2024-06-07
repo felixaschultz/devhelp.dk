ba2c90fcbab585205fa543e78b408191e18e3ed7
Felix A. Schultz <felix.schultz@engapho.com>
2024-06-07 23:41:18 +0200

    refactor: Update PostCard component to display user-specific information
    This commit updates the PostCard component to display user-specific information such as popularity score, views, and an edit link. It also adds a new CSS class to improve the styling of the component. These changes enhance the user experience and provide more functionality for managing posts.
    
    Refactor PostCard component to display user-specific information

--- app/components/PostCard/index.tsx
+++ app/components/PostCard/index.tsx
@@ -8,20 +8,22 @@ export default function PostCard({ post, user }) {
                 e.target.style.display = "none";
             }} loading="lazy" src={post.image} alt={post.title} />}
             <div className="post-card__content">
-                {(user?.user?._id == post.user || user?._id == post.user && post?.popularityScore) ? <p>Popularity Score: {post?.popularityScore}</p> : null}
+                <div className="grid">
+                    {(user?.user?._id == post.user || user?._id == post.user && post?.popularityScore) ? <p>Popularity Score: {post?.popularityScore}</p> : null}
+                    {
+                        (post.user == user?.user?._id || user?._id == post.user) ? (
+                            <div className="flex">
+                                <p>Views: {post?.views}</p>
+                                <Link style={{ color: "red", padding: "15px" }} to={`/blog/edit/${post?._id}`}>Edit</Link>
+                            </div>
+                        ) : null
+                    }
+                </div>
                 {post?.published || post?.public ? null : <p style={{ color: "red", marginTop: 0 }}>Draft</p>}
                 <h2>{post.title}</h2>
                 <div className="post-card-content" dangerouslySetInnerHTML={{
                     __html: post.body,
                 }}></div>
-                {
-                    (post.user == user?.user?._id || user?._id == post.user) ? (
-                        <>
-                            <p>Views: {post?.views}</p>
-                            <Link style={{ color: "red", padding: "15px" }} to={`/blog/edit/${post?._id}`}>Edit</Link>
-                        </>
-                    ) : null
-                }
             </div>
         </div>
     );
