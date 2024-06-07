14f10281eb3d07d928778462320c03f637e022f9
Felix A. Schultz <felix.schultz@engapho.com>
2024-06-07 23:38:32 +0200

    refactor: Update PostCard component styles and structure
    This commit refactors the styles and structure of the PostCard component. It updates the CSS classes and adds a new post-card__content div to encapsulate the post content. This change improves the readability and maintainability of the component.
    
    Refactor the PostCard component to improve styles and structure

--- app/components/PostCard/index.tsx
+++ app/components/PostCard/index.tsx
@@ -1,26 +1,28 @@
 import "./Style.css";
 import { Link } from "@remix-run/react";
 
-export default function PostCard({post, user}) {
+export default function PostCard({ post, user }) {
     return (
         <div className="post-card">
-            {(user?.user?._id == post.user || user?._id == post.user && post?.popularityScore) ? <p>Popularity Score: {post?.popularityScore}</p> : null}
-            {post?.published || post?.public ? null : <p style={{color: "red", marginTop: 0}}>Draft</p>}
             {post.image && <img onLoadStart={(e) => {
                 e.target.style.display = "none";
             }} loading="lazy" src={post.image} alt={post.title} />}
-            <h2>{post.title}</h2>
-            <div className="post-card-content" dangerouslySetInnerHTML={{
-                            __html: post.body,
-                        }}></div>
-            {
-                (post.user == user?.user?._id || user?._id == post.user) ? (
-                    <>
-                        <p>Views: {post?.views}</p>
-                        <Link style={{color: "red", padding: "15px"}} to={`/blog/edit/${post?._id}`}>Edit</Link>
-                    </>
-                ) : null
-            }
+            <div className="post-card__content">
+                {(user?.user?._id == post.user || user?._id == post.user && post?.popularityScore) ? <p>Popularity Score: {post?.popularityScore}</p> : null}
+                {post?.published || post?.public ? null : <p style={{ color: "red", marginTop: 0 }}>Draft</p>}
+                <h2>{post.title}</h2>
+                <div className="post-card-content" dangerouslySetInnerHTML={{
+                    __html: post.body,
+                }}></div>
+                {
+                    (post.user == user?.user?._id || user?._id == post.user) ? (
+                        <>
+                            <p>Views: {post?.views}</p>
+                            <Link style={{ color: "red", padding: "15px" }} to={`/blog/edit/${post?._id}`}>Edit</Link>
+                        </>
+                    ) : null
+                }
+            </div>
         </div>
     );
 }
