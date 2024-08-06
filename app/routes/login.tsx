7e2b0f8888168e6a8d50778808efe5470580ab92
Felix A. Schultz <felix.schultz@engapho.com>
2024-08-06 13:08:43 +0200

    refactor: Update login.tsx to improve decoding of token
    This commit updates the login.tsx file to improve the decoding of the token. It removes the unnecessary base64 decoding step and directly parses the token as JSON. This change simplifies the code and improves efficiency.
    
    Refactor login.tsx to improve decoding of token

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,4 +1,4 @@
-import { authenticator, oauthLogin} from "../services/auth.server";
+import { authenticator, oauthLogin } from "../services/auth.server";
 import mongoose from "mongoose";
 
 export const loader = async ({ request }) => {
@@ -7,18 +7,19 @@ export const loader = async ({ request }) => {
     const query = new URLSearchParams(new URL(info).search);
     const token = query.get('token');
     // Decode the token
-    const decodedToken = Buffer.from(token, 'base64').toString('utf-8');
+    /* const decodedToken = Buffer.from(token, 'base64').toString('utf-8'); */
 
     // Now you can use the decodedToken
-    const IntastellarAccount = JSON.parse(decodedToken);
+    const IntastellarAccount = JSON.parse(token);
+
     const foundAccount = await mongoose.models.User.findOne({
         linkedAccount: {
-            $elemMatch: {user_id: IntastellarAccount.user_id}
+            $elemMatch: { user_id: IntastellarAccount.user.user_id }
         }
     });
 
-    if(foundAccount){
-        foundAccount.image = IntastellarAccount.image;
+    if (foundAccount) {
+        foundAccount.image = IntastellarAccount.user.avatar;
         return await oauthLogin(foundAccount, {
             successRedirect: referrer,
             failureRedirect: referrer
